befea9c53b1d454d02198a7d43c582a86ae9cc3b
Dookie <me@djdookie.de>
2017-10-18 12:03:27 +0200

    Compatibility to Hearthstone Decktracker v1.5.1. Added comment.

--- Advisor/Advisor.cs
+++ Advisor/Advisor.cs
@@ -247,7 +247,7 @@ namespace HDT.Plugins.Advisor
                 // Create archetype dictionary
                 IDictionary<Deck, float> dict = new Dictionary<Deck, float>();
 
-                // Calculate similarities between all opponent's class archetype decks and all yet known opponent cards. Exclude wild decks in standard format using NAND expression.
+                // Calculate similarities between all opponent's class archetype decks and all yet known opponent cards. Exclude wild decks in standard format using NAND expression. OR expression should also work: (!d.IsWildDeck || CoreAPI.Game.CurrentFormat == Format.Wild)
                 foreach (var archetypeDeck in ArchetypeDecks.Where(d => d.Class == CoreAPI.Game.Opponent.Class && !(d.IsWildDeck && CoreAPI.Game.CurrentFormat == Format.Standard)))
                 {
                     // Insert deck with calculated value into dictionary and prevent exception by inserting duplicate decks
