ff36567224f2101635c0fb2b345851bbe516b35d
Jacob Bundgaard <jacob@jacobbundgaard.dk>
2020-04-29 00:34:42 +0200

    Update version to 1.0.11

--- Advisor/AdvisorPlugin.cs
+++ Advisor/AdvisorPlugin.cs
@@ -92,7 +92,7 @@ namespace HDT.Plugins.Advisor
         {
         }
 
-        public Version Version => new Version(1, 0, 10);
+        public Version Version => new Version(1, 0, 11);
 
         public async Task CheckForUpdate()
         {
