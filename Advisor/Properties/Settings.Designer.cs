00ff4ef8358ec7f8ed9fff34a93b0c704211c8f1
devance <andi.glaser85@gmail.com>
2020-04-29 15:02:42 +0200

    added "Remove played cards" option in settings and added a ScrollView

--- Advisor/Properties/Settings.Designer.cs
+++ Advisor/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Advisor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -260,6 +260,18 @@ namespace Advisor.Properties {
             }
             set {
                 this["MinimumSimilarity"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool RemovePlayedCards {
+            get {
+                return ((bool)(this["RemovePlayedCards"]));
+            }
+            set {
+                this["RemovePlayedCards"] = value;
             }
         }
     }
